8205da64ccf5f0396f950941482e9c09f88e726c
Thiago André <66754516+thiago1590@users.noreply.github.com>
2020-06-12 16:47:39 -0300

    Create motor_de_passo.ino

---
+++ codigo arduino/motor_de_passo/motor_de_passo.ino
@@ -0,0 +1,54 @@
+#include <Stepper.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_PRINT Serial
+
+Stepper my_Stepper(200, D8, D7, D6, D5);
+
+bool Right = false;
+bool Left = false;
+
+char auth[] = "Put your auth token here";
+char ssid[] = "Put your network name here";
+char pass[] = "Put your password here";
+
+void setup(){
+
+  Serial.begin(9600);                                
+  Blynk.begin(auth, ssid, pass);                     
+  my_Stepper.setSpeed(70);                            
+  
+}
+
+BLYNK_WRITE(V1){                                      
+  Right = param.asInt();                             
+  }
+  
+BLYNK_WRITE(V0){                                  
+  Left = param.asInt();                          
+  }
+  
+void Stepper1 (int Direction, int Rotation){       
+  for (int i = 0; i < Rotation; i++){                
+  my_Stepper.step(Direction * 200);               
+  Blynk.run();
+  }
+}
+
+void loop() {   
+  
+  Blynk.run();
+  
+  if (Right){                                        
+  Stepper1(1, 10);                                    
+  Serial.println("Right turn");
+  }
+  delay(20);                                      
+  
+  if (Left){                                          
+  Stepper1(-1, 10);                                   
+  Serial.println("Left turn");
+  }
+  delay(20);                                      
+}
